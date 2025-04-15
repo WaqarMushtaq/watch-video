f1da4243ddaad9e9737a9aa913f322931366a1e2
WaqarMushtaq <deepexplosion2@gmail.com>
2025-04-15 09:05:09 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const endpoint = https://www.profitableratecpm.com/p8a1a7zd4?key=ee9cde246c3149ef13fdf550ddc7914c
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
+					https://www.profitableratecpm.com/p8a1a7zd4?key=ee9cde246c3149ef13fdf550ddc7914c
 				}`,
 			},
 		};
